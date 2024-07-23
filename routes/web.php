ebb24e32e65e0394ba3997d38bf832908ae04b7d
Vu Tuan <tuanva.ba11-097@st.usth.edu.vn>
2024-07-23 18:10:50 +0700

    first update

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\HomeController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/',[HomeController::class,'index']);
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::get('/redirect',[HomeController::class,'redirect']);
